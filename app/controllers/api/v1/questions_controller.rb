f82a77299c3f8ee069dc41cb9211312b2e9f33ac
Kenton Archer <kenton8654@yahoo.com>
2020-11-07 13:51:17 -0500

    enabling cors

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,7 +2,25 @@ class Api::V1::QuestionsController < ApplicationController
   def index
     @questions = Question.all
 
-    render json: @questions status: ok
+    render json: @questions, status: :ok
 
   end
+
+  def show
+      @question = Question.find(params[:id])
+      render json: @question, status: :ok
+  end
+
+  def create
+
+  end
+
+
+
+
+def question_params
+  
+
+end
+
 end
